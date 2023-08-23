084632a749444b559ded2de7602107850342b22c
Leon <leon@leondreamed.com>
2023-08-23 11:20:30 -0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import https from "node:https";
 
 import { getMkcertCerts } from "@tunnel/mkcert";
 import { got as _got } from "got";
+import { getMonorepoDirpath } from "@tunnel/get-monorepo";
 
 import { gotErrorHook } from "./utils/error.js";
 
@@ -12,10 +13,16 @@ export const got = _got.extend({
         options.url !== undefined &&
         new URL(options.url).hostname.endsWith(".test")
       ) {
+        const monorepoDirpath = getMonorepoDirpath(import.meta.url);
+        if (monorepoDirpath === undefined) {
+          throw new Error("Could not find monorepo dirpath");
+        }
+
         const cliHelpersString = "@t/cli-helpers";
         const { cli } = await import(cliHelpersString);
         const { ca, cert, key } = await getMkcertCerts({
           mkcertBin: await cli.mkcert.getExecutablePath(),
+          monorepoDirpath,
         });
         options.agent.https = new https.Agent({
           ca,
