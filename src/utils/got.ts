dce8c28f5b76a9fba6354432317e702725333806
Leon Si <leon@leondreamed.com>
2023-10-20 11:34:50 -0400

    1.0.2

---
+++ src/utils/got.ts
@@ -0,0 +1,39 @@
+import https from "node:https";
+
+import { getMkcertCerts } from "@tunnel/mkcert";
+import { got as _got } from "got";
+import { getMonorepoDirpath } from "get-monorepo-root";
+import which from "which";
+
+import { gotErrorHook } from "./error.js";
+
+export const got = _got.extend({
+  handlers: [
+    async (options, next) => {
+      if (
+        options.url !== undefined &&
+        new URL(options.url).hostname.endsWith(".test")
+      ) {
+        const monorepoDirpath = getMonorepoDirpath(import.meta.url);
+        if (monorepoDirpath === undefined) {
+          throw new Error("Could not find monorepo dirpath");
+        }
+
+        const { ca, cert, key } = await getMkcertCerts({
+          mkcertBin: await which("mkcert"),
+          monorepoDirpath,
+        });
+        options.agent.https = new https.Agent({
+          ca,
+          cert,
+          key,
+        });
+      }
+
+      return next(options);
+    },
+  ],
+  hooks: {
+    beforeError: [gotErrorHook({})],
+  },
+});
